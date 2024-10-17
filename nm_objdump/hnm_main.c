5431254c83b377cc76237062eb32fc1488ed5a08
Sam <s.olubode@alustudent.com>
2024-10-17 11:40:07 +0100

    conditional hnm objfile

---
+++ nm_objdump/hnm_main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - main
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+        char SHELLSCRIPT[120] = "nm -p ";
+
+        if (argc < 2)
+        {
+                strcat(SHELLSCRIPT, "hnm_main");
+        }
+        else
+        {
+                strcat(SHELLSCRIPT, argv[1]);
+        }
+
+        system(SHELLSCRIPT);
+        return (0);
+}
