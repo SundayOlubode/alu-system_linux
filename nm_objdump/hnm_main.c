d15469818e1c97f634d6435c9cbab87dff1a9eb6
Sam <s.olubode@alustudent.com>
2024-10-17 13:37:15 +0100

    rename exec to hnm

--- nm_objdump/hnm_main.c
+++ nm_objdump/hnm_main.c
@@ -12,28 +12,24 @@
  */
 int main(int argc, char *argv[])
 {
-        // Ensure at least one object file is passed as an argument
-        if (argc < 2)
-        {
-                fprintf(stderr, "Usage: %s [objfile ...]\n", argv[0]);
-                return 1;
-        }
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s [objfile ...]\n", argv[0]);
+		return (1);
+	}
 
-        // Loop over each file passed as an argument
-        for (int i = 1; i < argc; i++)
-        {
-                char command[256] = "nm -p "; // Prepare the command string
+	for (int i = 1; i < argc; i++)
+	{
+		char command[256] = "nm -p ";
 
-                // Append the current file name to the command
-                strcat(command, argv[i]);
+		strcat(command, argv[i]);
 
-                // Execute the nm command with system()
-                if (system(command) != 0)
-                {
-                        fprintf(stderr, "Error: Failed to run nm on file %s\n", argv[i]);
-                        return 1;
-                }
-        }
+		if (system(command) != 0)
+		{
+			fprintf(stderr, "Error: Failed to run nm on file %s\n", argv[i]);
+			return (1);
+		}
+	}
 
-        return 0;
+	return (0);
 }
