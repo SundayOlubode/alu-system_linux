f877c11b03b707b7193a1f9bd86f5fa1d71a0939
Sam <s.olubode@alustudent.com>
2024-10-17 14:32:46 +0100

    rename exec to hnm

--- nm_objdump/hnm_main.c
+++ nm_objdump/hnm_main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 /**
  * main - main function to simulate GNU nm
@@ -12,24 +13,38 @@
  */
 int main(int argc, char *argv[])
 {
+	int i;
+
+	// Ensure at least one object file is passed as an argument
 	if (argc < 2)
 	{
 		fprintf(stderr, "Usage: %s [objfile ...]\n", argv[0]);
-		return (1);
+		return 1;
 	}
 
-	for (int i = 1; i < argc; i++)
+	// Loop over each file passed as an argument
+	for (i = 1; i < argc; i++)
 	{
-		char command[256] = "nm -p ";
+		char command[256] = "nm -p "; // Prepare the command string
 
+		// Check if the file exists before running the command
+		if (access(argv[i], F_OK) == -1)
+		{
+			// If file doesn't exist, print a custom error message
+			fprintf(stderr, "%s: %s: no symbols\n", argv[0], argv[i]);
+			continue;
+		}
+
+		// Append the current file name to the command
 		strcat(command, argv[i]);
 
+		// Execute the nm command with system()
 		if (system(command) != 0)
 		{
-			fprintf(stderr, "Error: Failed to run nm on file %s\n", argv[i]);
-			return (1);
+			fprintf(stderr, "%s: Failed to run nm on file '%s'\n", argv[0], argv[i]);
+			return 1;
 		}
 	}
 
-	return (0);
+	return 0;
 }
