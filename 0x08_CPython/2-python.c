699e5258a81a4deb762b44928da2f0d25dd7eaa9
Sam <s.olubode@alustudent.com>
2024-10-25 05:50:45 +0100

    2-python.c

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -22,21 +22,29 @@ void print_python_list(PyObject *py_list)
         }
 }
 
-void print_python_list(PyObject *p)
+void print_python_bytes(PyObject *p)
 {
-        PyListObject *list = (PyListObject *)p;
+        PyBytesObject *pb = (PyBytesObject *)p;
 
-        printf("[*] Python list info\n");
-        printf("[*] Size of the Python List = %ld\n", list->ob_base.ob_size);
-        printf("[*] Allocated = %ld\n", list->allocated);
-
-        for (Py_ssize_t i = 0; i < list->ob_base.ob_size; i++)
+        printf("[.] bytes object info\n");
+        if (PyBytes_Check(p))
         {
-                printf("Element %ld: %s\n", i, list->ob_item[i]->ob_type->tp_name);
+                printf("  size: %ld\n", pb->ob_base.ob_size);
+                printf("  trying string: %s\n", pb->ob_sval);
+                if (pb->ob_base.ob_size < 10)
+                        printf("  first %ld bytes:", pb->ob_base.ob_size + 1);
+                else
+                        printf("  first 10 bytes:");
 
-                if (PyBytes_Check(list->ob_item[i]))
+                for (int i = 0; i <= pb->ob_base.ob_size && i < 10; ++i)
+
                 {
-                        print_python_bytes(list->ob_item[i]);
+                        printf(" %02hhx", pb->ob_sval[i]);
                 }
+                printf("\n");
+        }
+        else
+        {
+                printf("  [ERROR] Invalid Bytes Object\n");
         }
 }
