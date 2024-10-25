583fd2a757c4c66bea9cad3476f02f7f6db7ca45
Sam <s.olubode@alustudent.com>
2024-10-25 05:49:11 +0100

    2-python.c

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -22,30 +22,21 @@ void print_python_list(PyObject *py_list)
         }
 }
 
-void print_python_bytes(PyObject *p)
+void print_python_list(PyObject *p)
 {
-        PyBytesObject *bytes = (PyBytesObject *)p;
-        Py_ssize_t size = 0;
+        PyListObject *list = (PyListObject *)p;
 
-        printf("[.] bytes object info\n");
-        if (!PyBytes_Check(p))
-        {
-                printf("  [ERROR] Invalid Bytes Object\n");
-                return;
-        }
+        printf("[*] Python list info\n");
+        printf("[*] Size of the Python List = %ld\n", list->ob_base.ob_size);
+        printf("[*] Allocated = %ld\n", list->allocated);
 
-        size = PyBytes_Size(p);
-        printf("  size: %zd\n", size);
-        printf("  trying string: %s\n", bytes->ob_sval);
-        if (size < 10)
-                printf("  first %zd bytes: ", size + 1);
-        else
-                printf("  first 10 bytes: ");
-        for (Py_ssize_t i = 0; i < size + 1 && i < 10; i++)
+        for (Py_ssize_t i = 0; i < list->ob_base.ob_size; i++)
         {
-                printf("%02hhx", bytes->ob_sval[i]);
-                if (i + 1 < size + 1 && i + 1 < 10)
-                        printf(" ");
+                printf("Element %ld: %s\n", i, list->ob_item[i]->ob_type->tp_name);
+
+                if (PyBytes_Check(list->ob_item[i]))
+                {
+                        print_python_bytes(list->ob_item[i]);
+                }
         }
-        printf("\n");
 }
